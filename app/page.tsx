9ae89c900f6c65ee5aa532d1d8d8b938af330855
notjeremie <117147192+notjeremie@users.noreply.github.com>
2025-06-30 13:06:19 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
 "use client"
+export const dynamic = 'force-static'  // Force le rendu statique
+
+export default function Home() {
+  // Votre code de countdown...
+}
 
 import CountdownTimer from "../countdown-timer"
 
