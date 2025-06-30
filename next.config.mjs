e3bc1f8fdded7c8cfe4958bab576fd65ae489431
notjeremie <117147192+notjeremie@users.noreply.github.com>
2025-06-30 12:05:44 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  distDir: 'out',
+  trailingSlash: true,
   images: {
-    unoptimized: true,
-  },
+    unoptimized: true
+  }
 }
 
 export default nextConfig
